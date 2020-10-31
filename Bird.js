42d9b1b98167156ed12868a2482c39e062f44940
prisha <prishaagr16@gmail.com>
2020-10-31 10:45:10 +0530

    Slingshot

--- Bird.js
+++ Bird.js
@@ -3,5 +3,10 @@ class Bird extends BaseClass{
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");  
     }
+    display(){
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+      super.display();
+    }
   }
   
--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1,backgroundImage;
-var platform;
+var platform,slingshot;
 
 function preload(){
 backgroundImage = loadImage("sprites/bg.png");
@@ -32,17 +32,15 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(230,150,180,PI/2);
     bird = new Bird(100,100);
-
+    slingshot = new Slingshot(bird.body,log6.body);
 }
 
 function draw(){
     background(backgroundImage);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    
     box1.display();
     box2.display();
     ground.display();
@@ -57,8 +55,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
     bird.display();
 
     platform.display();
+    slingshot.display();
 }
