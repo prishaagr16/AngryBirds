bf42b0e330226b4d1f6f35f3423b6bd87e60f618
prisha <prishaagr16@gmail.com>
2020-11-12 16:15:26 +0530

    edited

--- Pig.js
+++ Pig.js
@@ -2,11 +2,7 @@ class Pig extends BaseClass{
     constructor(x, y) {
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
-<<<<<<< HEAD
-    this.Visibility = 255;
-=======
             this.Visibility = 255;
->>>>>>> 9fd700bfbec424d67076597a5cf8e0aecb0fc1a4
     }
     display(){
       //console.log(this.body.speed);
@@ -15,16 +11,8 @@ class Pig extends BaseClass{
       }
       else{
         World.remove(world,this.body);
-<<<<<<< HEAD
-        push();
-        this.Visibility = this.Visibility - 5;
-        tint(255,this.Visibility);
-        image(this.image,this.body.position.x,this.body.position.y,50,50);
-        pop();
-=======
           tint(255,this.Visibility);
         image(this.image,this.body.position.x,this.body.position.y,50,50);
->>>>>>> 9fd700bfbec424d67076597a5cf8e0aecb0fc1a4
       }
     }
   };
